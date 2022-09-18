db494ded41dba9084c661fd635b9d61562f34b45
tarasfl <tfliorko@gmail.com>
2022-09-18 15:42:05 +0300

    f

--- js/dom_util.js
+++ js/dom_util.js
@@ -1,12 +1,36 @@
 const amount_of_pages = document.getElementById('amount_of_pages')
 const author = document.getElementById('author')
 const price = document.getElementById('price')
+const right_bar = document.getElementById('books_content')
+
+
+const get_id = (id) => `item-${id}`;
+
+const item_template = ({id, amount_of_pages, author, price}) => `
+    <div id='${get_id(id)}' class = 'book'>
+        <p>Amount of pages: ${amount_of_pages}</p>
+        <p>Author: ${author}</p>
+        <p>Price: ${price}</p>
+    </div>
+`;
+
 
 export const get_input = () => {
     return {
         amount_of_pages: amount_of_pages.value,
         author: author.value,
         price: price.value
-    }
-
+    }   
 }
+
+export const clear_input = () => {
+    amount_of_pages.value = ''
+    author.value = ''
+    price.value = ''
+};
+export const add_item_to_html = ({id, amount_of_pages, author, price}) => {
+    right_bar.insertAdjacentHTML (
+        'afterbegin', 
+        item_template({id, amount_of_pages, author, price})
+    )
+} 
--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
-alert('jhsjd')
+import {
+    get_input,
+    clear_input,
+    add_item_to_html
+}from './dom_util.js'
 const search_button = document.getElementById('search_button');
 const clear_button = document.getElementById('clear_button');
+const create_button = document.getElementById('create_button')
+
+let books = []
 
 
+const add_item  = ({amount_of_pages, author, price}) => {
+    const generated_id  =   uuid.v1()
+    const new_item = {
+        id: generated_id,
+        amount_of_pages: amount_of_pages,
+        author: author,
+        price: price 
+    }
 
-search_button.addEventListener('click', () => {console.log('njnjcx')});
+    books.push(new_item)
+
+    add_item_to_html(new_item)
+} 
+
+create_button.addEventListener('click', (event) => 
+{
+    event.preventDefault();
+    const {amount_of_pages, author, price} = get_input();
+    console.log(amount_of_pages)
+    clear_input();
+    add_item({amount_of_pages, author, price})
+});
